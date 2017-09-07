4a6c583329afffaf9cbf5a21fa803c415683e885
nikrb <nik_rb@yahoo.com>
2017-09-07 18:47:54 +0100

    fix the weekday demo to work with d3 v4

--- src/components/charts/Axis.js
+++ src/components/charts/Axis.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as d3 from 'd3';
+import weekday from '../../weekday.js';
 
 export default class Axis extends React.Component {
   componentDidMount = () => {
@@ -10,7 +11,10 @@ export default class Axis extends React.Component {
   };
   renderAxis = () => {
     if( this.props.orient === "horiz"){
-      const haxis = d3.axisBottom( this.props.scale);
+      const dateFormat = d3.timeFormat( "%Y-%m-%d");
+      const haxis = d3.axisBottom( this.props.scale)
+        .tickFormat(function (d) { return dateFormat(weekday.invert(d)); });
+
       // FIXME: this.refs.axis isn't working here
       haxis( d3.select( '.horiz'));
     } else {
--- src/components/charts/LineChart.js
+++ src/components/charts/LineChart.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import * as d3 from 'd3';
 import Line from './Line';
 import XYAxis from './XYAxis';
-import scaleDayselect from '../../dayselect.js';
 import weekday from '../../weekday';
 
 export default class LineChart extends React.Component {
@@ -22,28 +21,35 @@ export default class LineChart extends React.Component {
     const parseTime = d3.timeParse("%Y-%m-%d");
 
     const weekday_data = data.map( (d) => {
-      return { date: parseTime( d.date), weekday: weekday( d.date), close: d.value};
+      return { date: parseTime( d.date), weekday: weekday( parseTime( d.date)),
+        close: d.value};
     });
+
+    console.log( "weekday data:", weekday_data);
+
+    // this goes in axis
+    // const date_format = d3.timeFormat( "%Y-%m-%d");
+    // xAxis .tickFormat(function (d) { return dateFormat(weekday.invert(d)); });
 
     // set the ranges
     // const xScale = d3.scaleTime().range([0, inner_width]);
-
-    const weekday_extent = d3.extent(weekday_data, function(d) { return d.weekday; });
-    const xScale = scaleDayselect( weekday, xScale).range([0, inner_width]);
+    // const xScale = scaleDayselect( weekday).range([0, inner_width]);
+    const xScale = d3.scaleLinear().range( [0, inner_width]);
     const yScale = d3.scaleLinear().range([inner_height, 0]);
 
     // xScale.domain(d3.extent(fd, function(d) { return d.date; }));
-    xScale.domain(weekday_extent);
-    yScale.domain([d3.min(weekday_data, d => d.close), d3.max(weekday_data, d => d.close )]);
+    xScale.domain( d3.extent(weekday_data, function(d) { return d.weekday; }));
+    // yScale.domain([d3.min(weekday_data, d => d.close), d3.max(weekday_data, d => d.close )]);
+    yScale.domain( d3.extent( weekday_data, d => parseFloat( d.close)));
 
     const lines = <Line data={weekday_data} xScale={xScale} yScale={yScale}
-          fill="transparent" stroke="black"/>;
+          stroke="black"/>;
 
-    const dots = weekday_data.map( (line,i) => {
-      return line.map( (l,j) => {
-        return <circle key={i*100+j} cx={xScale(l.date)} cy={yScale(l.close)}
+        const dots = weekday_data.map( (d,i) => {
+      // return line.map( (l,j) => {
+        return <circle key={i} cx={xScale(d.weekday)} cy={yScale(d.close)}
           r="3" fill="red" />;
-      });
+      // });
     });
     return (
       <svg width={width} height={height}>
